b3f58a055330a6212a416da7a6314924fa65ecd5
ysh <yash110704@gmail.com>
2025-01-02 13:56:31 +0530

    changed some things

--- init.lua
+++ init.lua
@@ -469,6 +469,13 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'startup-nvim/startup.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-file-browser.nvim' },
+    config = function()
+      require('startup').setup { theme = 'dashboard' }
+    end,
+  },
   -- LSP Plugins
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
